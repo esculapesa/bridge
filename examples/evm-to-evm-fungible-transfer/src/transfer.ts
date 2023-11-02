f9aae413e38a40f3e6c7738bf8720ac65de325b4
FSM1 <myankelev@gmail.com>
2023-11-02 16:15:20 +0100

    increase amount

--- examples/evm-to-evm-fungible-transfer/src/transfer.ts
+++ examples/evm-to-evm-fungible-transfer/src/transfer.ts
@@ -98,7 +98,7 @@ export async function erc20Transfer(): Promise<void> {
     SEPOLIA_CHAIN_ID,
     await wallet.getAddress(), // Sending to the same address on a different chain
     RESOURCE_ID,
-    "500000000000000000" // 18 decimal places
+    "5000000000000000000" // 18 decimal places
   );
 
   const fee = await assetTransfer.getFee(transfer);
